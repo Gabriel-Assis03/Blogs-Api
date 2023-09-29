5e90dcb54d079199736155ae09c94a862b3f0a93
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-09-29 18:34:38 -0300

    req 11 full

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+      postId: {
+        type: DataTypes.INTEGER, 
+        foreignKey: true
+      },
+      categoryId: {
+        type: DataTypes.INTEGER, 
+        foreignKey: true
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'post_categories',
+      underscored: true,
+    },
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'books',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
